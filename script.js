f04e406fdb39d8daa7b41d1ec0e1fc2b987d863c
vyijardar <vishakhayijardar@gmail.com>
2024-09-18 21:21:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-
-const form = document.querySelector('#form')
-const submitButton = document.querySelector('#submit')
-
-form.addEventListener('submit', (e) => {
-  submitButton.disabled = true
-  e.preventDefault()
-  window.location.href = window.location.origin + '/success.html'
-})
-
-alert("Your Form is Successfully Submitted.");
+console.log("Hello World");
